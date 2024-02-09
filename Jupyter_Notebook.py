10b6347bd0f3829c5748ed979d51e6657634ddd2
asadzaheer1 <32482825+asadzaheer1@users.noreply.github.com>
2024-02-09 12:23:16 -0800

    Add files via upload

---
+++ Jupyter_Notebook.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo">
+# </center>
+# 
+
+# #### Add your code below following the instructions given in the course
+# 
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. **Python:** Renowned for its simplicity and versatility, Python is widely adopted in data science for tasks such as data analysis, machine learning, and scripting.
+# 
+# 2. **R:** Particularly popular in statistical analysis and academia, R is a powerful language for data visualization, statistical modeling, and exploratory data analysis.
+# 
+# 3. **SQL:** A fundamental language for working with relational databases, SQL (Structured Query Language) is crucial for data manipulation, querying, and database management in data science projects.
+# 
+# 4. **Julia:** Gaining traction for its speed and performance, Julia is used in numerical and scientific computing, offering a good balance between simplicity and high-performance computing.
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. **NumPy:** A fundamental library for numerical computing in Python, NumPy provides support for large, multi-dimensional arrays and matrices, along with mathematical functions.
+# 
+# 2. **Pandas:** Built on top of NumPy, Pandas offers high-performance, easy-to-use data structures for data manipulation and analysis, particularly with structured data.
+# 
+# 3. **Scikit-learn:** A comprehensive machine learning library for Python, Scikit-learn provides tools for data mining and data analysis, along with various algorithms for machine learning tasks.
+# 
+# 4. **Matplotlib:** A versatile plotting library in Python, Matplotlib is widely used for creating static, animated, and interactive visualizations in data science.
+
+# | Development Environment Tools |
+# | ----------------------------- |
+# | Jupyter Notebook              |
+# | RStudio                       |
+# | VS Code with Python Extension |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[3]:
+
+
+# Convert 200 minutes to hours by dividing by 60
+minutes = 200
+hours = minutes / 60
+print(hours)
+
+
+# # Objectives:
+# 
+# **Key Takeaways from the Notebook:**
+# 
+# - **Explore Data Science Tools:** Understand the diverse landscape of data science tools and the broader ecosystem.
+# 
+# - **Identify Popular Languages:** List and recognize popular programming languages commonly used in Data Science.
+# 
+# - **Introduction to Key Libraries:** Gain familiarity with commonly used libraries in Data Science, such as NumPy, Pandas, and Scikit-learn.
+# 
+# - **Anaconda in Data Science:** Understand the role of Anaconda as a platform for data science workflows and package management.
+# 
+# 
+# 
+
+# ## Author
+# 
+# Asad Zaheer
+
+# In[ ]:
+
+
+
+
